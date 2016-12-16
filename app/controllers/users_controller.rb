6b2eb621c0f2990799a543cfbf639585e8f4c992
Charles-Xiao <xiaoy24charles@gmail.com>
2016-12-16 12:25:11 +0800

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -17,14 +18,14 @@ class UsersController < ApplicationController
   end
 
   def create
-      @user = User.new(user_params)
-	    if @user.save
-		    @user.send_activation_email
-        flash[:info] = "Please check your email to activate your account."
-        redirect_to root_url
-	    else
-         render 'new'
-      end
+    @user = User.new(user_params)
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -49,25 +50,18 @@ class UsersController < ApplicationController
 
   private
 
-  	def user_params
-  	 	 params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-  	end
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in"
-        redirect_to login_url
-      end
-    end
-    
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
-    end
-    
-    def admin_user
-      redirect_to(root_url) unless current_user.admin?
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214073619) do
+ActiveRecord::Schema.define(version: 20161216034812) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
-    t.string   "user_id"
-    t.string   "integer"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "picture"
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
