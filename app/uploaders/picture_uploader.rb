222013b99d288d244e5821be1d0f16d372558ca4
Charles-Xiao <xiaoy24charles@gmail.com>
2016-12-16 12:38:24 +0800

    bug fixed

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,7 +7,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
 
-  if Rails.enc.production?
+  if Rails.env.production?
     storage :fog
   else
     storage :file
